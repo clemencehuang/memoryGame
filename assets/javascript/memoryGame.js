feec7f56c1b37fe8c79110b4a193d9a84b2664ca
clemencehuang <153006617+clemencehuang@users.noreply.github.com>
2024-05-15 12:47:57 +0100

    updated displayBoard function, added flipCard function

--- assets/javascript/memoryGame.js
+++ assets/javascript/memoryGame.js
@@ -3,76 +3,116 @@
 cards= []
 board = []
 
+/* -------------- fill card deck with 2x 8 images ------------- */
 
-function fillCardDeck(){
+function fillCardDeck() {
 
-    for (let j = 1; j <= 2 ; j++){
+    for (let j = 1; j <= 2 ; j++){ // iterate through below loop 2x (i.e. 2x 8 images)
        for (let i = 1; i <= 8 ; i++){
-            let imageName = "assets/images/img".concat(i, ".PNG");
+            let imageName = "assets/images/img-".concat(i, ".PNG");
             cards.push(imageName);
         }
     }
 }
 
-function fillBoard(){
+
+
+/* -------------- fill board play area randomly with those 16 cards ------------- */
+
+function fillBoard() {
     for (i = 1 ; i <=16 ; i++) {
 
         let randNumber = Math.floor(Math.random() * cards.length );
 
-        board.push(cards[randNumber]);
-        cards.splice(randNumber, 1);
+        board.push(cards[randNumber]); // push random card onto the board
+        cards.splice(randNumber, 1); // remove that random card from array so that it doesn't get chosen
     }
 }
 
+
+
+/* -------------- link board play area to html ------------- */
+
+    // ORIGINAL HTML WITH ITS CLASSES:
+    //
+    //   <ul class="cards">
+    //     <li class="card">
+    //       <div class="view front-view"> <!-- b/w patterned cards -->
+    //         <img src="assets/images/que_icon-1.png" alt="icon">
+    //       </div>
+    //       <div class="view back-view"> <!-- coloured cards -->
+    //         <img src="assets/images/img-1.png" alt="card-img">
+    //       </div>
+    //     </li>
 
 function displayBoard() {
 
+/*  I changed the name to playAreaBackView as these cards are showing the 
+    BACK VIEW = coloured patterns BUT also need to show the FRONT VIEW = b/w pattern */
+    
+    let playAreaBackView = "<ul class='cards'>";
 
-let playArea = "<div id='container'> ";
+    playAreaBackView += "<li class='card'>";
 
+    for (j = 0; j <= 15; j++) {
 
-
-
-playArea += "<div id='row'>";
-
-
-    for (j = 0 ; j <=15 ; j++) {
-
-        playArea += "<button onclick='checkAnswer()'><img class='image' src="
-        playArea += board[j]
-        playArea += "></button>"
+        // I changed it from onclick='checkAnswer() to ==> onclick='matchCards()
+        playAreaBackView += "<button onclick='matchCards()'><img class='view' src="
+        playAreaBackView += board[j]
+        playAreaBackView += "></button>"
 
     }
 
-    playArea += "</div>";
+    playAreaBackView += "</ul>";
 
-
-
-
-    console.log(playArea)
-
-    document.getElementById('board').innerHTML = playArea;
-
-
+    document.getElementById('board').innerHTML = playAreaBackView;
 }
 
-function checkAnswer(){
 
-    // check that 2 cards match
+
+/* -------------- flip cards ------------- */
+
+let cardOne, cardTwo; // define two cards that need to be selected
+
+    function flipCard(e) { // e = event
+        let clickedCard = e.target; // target = click event that happened on the clickedCard
+        clickedCard.classList.add("flip");
+        cardOne = clickedCard;
+        cardTwo = clickedCard;
+
+    }
+
+cards.forEach(card => { // adding click even to all cards
+    card.addEventListener("click", flipCard); // when cards are clicked, function clickCard takes place
+});
+
+
+
+/* -------------- match cards ------------- */
+
+function matchCards() {
+
 }
 
 
 
 /* -------------- main processing ------------- */
+
 console.log("before CARDS", cards);
 console.log("before BOARD", board);
 fillCardDeck();
 
 console.log("middle CARDS", cards);
-console.log("middle BOARD",board);
+console.log("middle BOARD", board);
 
 fillBoard();
 console.log("after CARDS", cards);
-console.log("after BOARD",board);
+console.log("after BOARD", board);
 
 displayBoard();
+console.log("after CARDS", cards);
+console.log("after BOARD", board);
+console.log(playAreaBackView);
+
+flipCard(e);
+console.log(cardOne, cardTwo);
